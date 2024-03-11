853d6d450caef7e3ccb10e74f453e71e888dac01
MandichRiccardo <ricky.mandich@gmail.com>
2024-03-11 16:13:14 +0100

    creazione 2024 03 11 16:12

---
+++ main.py
@@ -0,0 +1,27 @@
+# esercizio 1
+numero1 = 10
+numero2 = 5
+somma = numero1 + numero2
+print(somma)
+# esercizio 2
+differenza = numero1 - numero2
+print(differenza)
+# esercizio 3
+prodotto = numero1 * numero2
+print(prodotto)
+# esercizio 4
+quoziente = numero1 / numero2
+print(quoziente)
+# esercizio 5
+resto = numero1 % numero2
+print(resto)
+# esercizio 6
+numero1 += 1
+numero2 -= 3
+print(numero1, numero2)
+# esercizio 7
+numero1 *= numero1 + 5
+risultato = numero1 + numero2 ** 2
+print(risultato)
+# esercizio 8
+numero1 -= numero1 - 1
